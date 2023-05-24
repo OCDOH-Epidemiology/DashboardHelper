a10f6cfb06980addb38fc19c97a1958d4e3bf7b1
SRubinOCDOH <104148508+SRubinOCDOH@users.noreply.github.com>
2023-05-24 12:16:50 -0400

    Created code generation for adding in new datasets.

--- R/mod_new_data_set.R
+++ R/mod_new_data_set.R
@@ -10,7 +10,7 @@
 mod_new_data_set_ui <- function(id){
   ns <- NS(id)
   tagList(
-    tags$h1("New data set!")
+    shiny::uiOutput(ns("main"))
   )
 }
     
@@ -20,7 +20,31 @@ mod_new_data_set_ui <- function(id){
 mod_new_data_set_server <- function(id, section_selection){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
+    output$main <- renderUI({
+      req(section_selection())
+
+      if (section_selection() != "New Data Set") {
+        return()
+      }
+
+      tagList(
+      shiny::textInput(ns("sheet_name"), "Excel Sheet Name"),
+      shiny::textInput(ns("variable_name"), "Optional: Variable Name"),
+      shiny::verbatimTextOutput(ns("generated_code"))
+      )
+    })
+
+    output$generated_code <- shiny::renderPrint({
+      var_name <- gsub(" ", "_", tolower(ifelse(input$variable_name == "", input$sheet_name, input$variable_name)))
+
+      cat(
+        "# ", input$sheet_name, "\n",
+        var_name, " <- readxl::read_xlsx(source, sheet = \"", input$sheet_name,"\", skip = 1)\n",
+        "usethis::use_data(", var_name, ", overwrite = TRUE)",
+        sep = ""
+      )
+    })
   })
 }
     
